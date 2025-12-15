94685f2ce53e2430cba50f8e42c11da4149cc1c0
Andy Browning <andybrowning5@gmail.com>
2025-12-15 13:24:59 -0500

    code review changes

--- apps/frontend/src/pages/home/home.tsx
+++ apps/frontend/src/pages/home/home.tsx
@@ -2,11 +2,11 @@ import { FeaturedCoursesSection } from "@components/FeaturedCourses";
 import styles from "./home.module.css";
 
 function Home() {
-  return( 
+  return (
     <div className={styles.page}>
       <FeaturedCoursesSection />
     </div>
-    );
+  );
 }
 
 export default Home;
