24b0379af3e87b9d5526d10acd9c7d57600169c6
Musharab Amin <amin.musharab@gmail.com>
2025-12-15 12:17:31 -0500

    Fix: change react-router to react-router-dom in imports

--- apps/frontend/src/components/best-selling-courses/course-card.tsx
+++ apps/frontend/src/components/best-selling-courses/course-card.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import styles from "./best-selling-courses.module.scss";
 import type { BestSellingCourse } from "@/types/course";
 
